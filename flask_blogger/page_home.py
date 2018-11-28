74364681c45a0ee9e33f19fe524efe1fa9135307
langdahu <123456@qq.com>
2018-11-28 12:58:08 +0800

    del css

--- flask_blogger/page_home.py
+++ flask_blogger/page_home.py
@@ -7,11 +7,7 @@ page_bp = Blueprint('page',
 @page_bp.route('/imgs')
 def imgs_d():
     return render_template('page/test.html')
-
-@page_bp.route('/login')
-def page_login():
-    return render_template('page/page-login.html')
-
+    
 @page_bp.app_errorhandler(404)
 def page_not_found(error):
     return render_template('page/404.html')
